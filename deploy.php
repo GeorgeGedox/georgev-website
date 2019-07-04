88e7b6a558d6c211a863ab66889de2f04c5f4771
GeorgeGedox <39589642+GeorgeGedox@users.noreply.github.com>
2019-07-04 20:42:46 +0300

    Added CI&CD support
    Signed-off-by: GeorgeGedox <39589642+GeorgeGedox@users.noreply.github.com>

---
+++ deploy.php
@@ -0,0 +1,32 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+set('application', 'georgev-website');
+set('repository', 'git@github.com:GeorgeGedox/georgev-website.git');
+
+set('git_tty', false);
+set('allow_anonymous_stats', false);
+
+set('deploy_server', null);
+
+// Hosts
+host('Production Server')
+    ->hostname('{{ deploy_server }}')
+    ->user('deployer')
+    ->port(22)
+    ->forwardAgent(true)
+    ->addSshOption('UserKnownHostsFile', '/dev/null')
+    ->addSshOption('StrictHostKeyChecking', 'no')
+    ->stage('production')
+    ->roles('app')
+    ->set('deploy_path', '/var/www/georgev.design')
+    ->set('branch', 'master')
+    ->set('http_user', 'www-data');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
