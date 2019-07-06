cf171665898a2686c62a16451a8e8b711fee87be
GeorgeGedox <39589642+GeorgeGedox@users.noreply.github.com>
2019-07-06 20:16:27 +0300

    Flexible settings panel, helper, model tests & fixes
    Signed-off-by: GeorgeGedox <39589642+GeorgeGedox@users.noreply.github.com>

---
+++ app/Setting.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+class Setting extends Model
+{
+    public $incrementing = false;
+
+    protected $primaryKey = 'key';
+    protected $keyType = 'string';
+    protected $fillable = ['key', 'value', 'type'];
+
+    /**
+     * Update or create a setting
+     *
+     * @param string $key
+     * @param $value
+     * @param string $type
+     * @return bool
+     * @throws \Exception
+     */
+    public static function add(string $key, $value, string $type = 'string')
+    {
+        if (self::has($key)) {
+            return self::set($key, $value, $type);
+        }
+
+        return self::create([
+            'key' => $key,
+            'value' => self::castValueSetter($value, $type),
+            'type' => $type
+        ]) ? true : false;
+    }
+
+    /**
+     * Check if setting exists by key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public static function has(string $key)
+    {
+        return (boolean)self::getAllSettings()->find($key);
+    }
+
+    /**
+     * Update or create a setting
+     *
+     * @param string $key
+     * @param $value
+     * @param string $type
+     * @return bool
+     * @throws \Exception
+     */
+    public static function set(string $key, $value, string $type = 'string')
+    {
+        if ($setting = self::getAllSettings()->find($key)) {
+            return $setting->update([
+                'key' => $key,
+                'value' => self::castValueSetter($value, $type),
+                'type' => $type
+            ]) ? true : false;
+        }
+
+        return self::add($key, $value, $type);
+    }
+
+    /**
+     * Remove a setting from the database
+     *
+     * @param string $key
+     * @return bool
+     */
+    public static function remove(string $key)
+    {
+        if (self::has($key)) {
+            return self::find($key)->delete();
+        }
+
+        return false;
+    }
+
+    /**
+     * Get setting by key or return null
+     *
+     * @param string $key
+     * @return bool|false|int|null|string
+     */
+    public static function get(string $key)
+    {
+        if (self::has($key)) {
+            $setting = self::getAllSettings()->find($key);
+            return self::castValueGetter($setting->value, $setting->type);
+        }
+
+        return null;
+    }
+
+    /**
+     * Cast value to type
+     *
+     * @param $value
+     * @param $castTo
+     * @return bool|false|int|string
+     */
+    public static function castValueGetter($value, $castTo)
+    {
+        switch ($castTo) {
+            case 'int':
+            case 'integer':
+                return intval($value);
+                break;
+
+            case 'bool':
+            case 'boolean':
+                return boolval($value);
+                break;
+
+            case 'array':
+                return json_decode($value, true);
+                break;
+
+            default:
+                return $value;
+                break;
+        }
+    }
+
+    /**
+     * Cast value to type for database inserting
+     *
+     * @param $value
+     * @param $castTo
+     * @return bool|false|int|string
+     * @throws \Exception
+     */
+    public static function castValueSetter($value, $castTo)
+    {
+        switch ($castTo) {
+            case 'int':
+            case 'integer':
+                return intval($value);
+                break;
+
+            case 'bool':
+            case 'boolean':
+                return boolval($value);
+                break;
+
+            case 'array':
+                if (!is_array($value)){
+                    throw new \Exception("Did not receive array value.");
+                }
+                return json_encode($value);
+                break;
+
+            default:
+                return $value;
+                break;
+        }
+    }
+
+    /**
+     * Get all settings
+     *
+     * @return mixed
+     */
+    public static function getAllSettings()
+    {
+        return Cache::rememberForever('settings.all', function (){
+            return self::all();
+        });
+    }
+
+    /**
+     * Flush cache
+     *
+     */
+    public static function flushCache(){
+        Cache::forget('settings.all');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Flush cache on update
+        static::updated(function (){
+            self::flushCache();
+        });
+
+        // Flush cache on create
+        static::created(function (){
+            self::flushCache();
+        });
+    }
+}
--- resources/views/dashboard/settings/general/index.blade.php
+++ resources/views/dashboard/settings/general/index.blade.php
@@ -8,6 +8,11 @@
 
     <div class="container-fluid mt--7">
         <div class="row">
+            <div class="col-12">
+                @include('dashboard.partials.alert')
+            </div>
+        </div>
+        <div class="row">
             <div class="col-lg-6">
                 <div class="card bg-secondary shadow">
                     <div class="card-header bg-white border-0">
@@ -16,9 +21,45 @@
                         </div>
                     </div>
                     <div class="card-body">
+                        <h6 class="heading-small text-muted mb-2">{{ __('SEO') }}</h6>
+                        <div class="pl-lg-3">
+                            <form method="post" action="{{ route('dashboard.settings.general.seo') }}" autocomplete="off">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <label class="form-control-label">{{ __('Homepage title') }}</label>
+                                        <div class="form-group{{ $errors->has('title') ? ' has-danger' : '' }}">
+                                            <input type="text" name="title" class="form-control form-control-alternative{{ $errors->has('title') ? ' is-invalid' : '' }}"
+                                                   placeholder="{{ __('Title') }}"
+                                                   value="{{ old('title', setting('general_seo_title')) }}">
 
+                                            @if ($errors->has('title'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('title') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <label class="form-control-label">{{ __('Meta description') }}</label>
+                                        <div class="form-group{{ $errors->has('title') ? ' has-danger' : '' }}">
+                                            <textarea name="description" rows="5" class="form-control form-control-alternative{{ $errors->has('description') ? ' is-invalid' : '' }}"
+                                                      placeholder="{{ __('Describe your website') }}">{{ old('description', setting('general_seo_description')) }}</textarea>
+
+
+                                            @if ($errors->has('title'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('title') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-success mt-2">{{ __('Save') }}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                         <hr>
-                        <h6 class="heading-small text-muted mb-4">{{ __('Maintenance Mode') }}</h6>
+                        <h6 class="heading-small text-muted mb-2">{{ __('Maintenance Mode') }}</h6>
                         <div class="pl-lg-3">
                             <form method="post" action="{{ route('dashboard.settings.general.maintenance') }}" autocomplete="off">
                                 @csrf
@@ -38,9 +79,9 @@
                                         </div>
                                         <div class="text-center">
                                             @if(app()->isDownForMaintenance())
-                                                <button type="submit" class="btn btn-danger mt-4">{{ __('Disable') }}</button>
+                                                <button type="submit" class="btn btn-danger mt-2">{{ __('Disable') }}</button>
                                             @else
-                                                <button type="submit" class="btn btn-success mt-4">{{ __('Enable') }}</button>
+                                                <button type="submit" class="btn btn-success mt-2">{{ __('Enable') }}</button>
                                             @endif
                                         </div>
                                     </div>
@@ -58,7 +99,88 @@
                         </div>
                     </div>
                     <div class="card-body">
+                        @if ($errors->has('social.*'))
+                            <div class="alert alert-danger">
+                                <ul class="list-unstyled" style="margin: 0">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form id="socialForm" method="post" action="{{ route('dashboard.settings.general.social') }}" autocomplete="off">
+                            @csrf
+                            <div class="social-input-wrapper">
+                                @forelse(setting('general_social', []) as $index => $item)
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <input type="text" name="social[{{ $index }}][name]" class="form-control form-control-alternative" value="{{ $item['name'] }}"
+                                                       placeholder="{{ __('Name') }}" data-lpignore="true">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <input type="text" name="social[{{ $index }}][icon]" class="form-control form-control-alternative" value="{{ $item['icon'] }}"
+                                                       placeholder="{{ __('Icon') }}" data-lpignore="true">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <input type="text" name="social[{{ $index }}][url]" class="form-control form-control-alternative" value="{{ $item['url'] }}"
+                                                       placeholder="{{ __('URL') }}" data-lpignore="true">
+                                            </div>
+                                        </div>
+                                    </div>
+                                @empty
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <input type="text" name="social[0][name]" class="form-control form-control-alternative" placeholder="{{ __('Name') }}" data-lpignore="true">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <input type="text" name="social[0][icon]" class="form-control form-control-alternative" placeholder="{{ __('Icon') }}" data-lpignore="true">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <input type="text" name="social[0][url]" class="form-control form-control-alternative" placeholder="{{ __('URL') }}" data-lpignore="true">
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforelse
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-success mt-2">{{ __('Save') }}</button>
+                                        <button type="button" class="btn btn-primary mt-2 add-field">{{ __('Add field') }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                            @push('js')
+                                <script>
+                                    $(document).ready(function () {
+                                        const $form = $('#socialForm');
+                                        const $wrapper = $('.social-input-wrapper');
 
+                                        $form.find('button.add-field').on('click', function () {
+                                            let $clone = $wrapper.find('>.row:first-child').clone(true).appendTo($wrapper);
+                                            $clone.find('input').val('');
+
+                                            $wrapper.find('.row').each(function (index) {
+                                                let prefix = "social[" + index + "]";
+                                                $(this).find("input").each(function () {
+                                                    this.name = this.name.replace(/social\[\d+\]/, prefix);
+                                                });
+                                            });
+                                        });
+                                    });
+                                </script>
+                            @endpush
+                        </form>
                     </div>
                 </div>
             </div>
